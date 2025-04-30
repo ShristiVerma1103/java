b4f69c80702f59210be568707c8eaea7549a151c
ShristiVerma1103 <shristiverma1103@gmail.com>
2025-04-30 20:13:45 +0530

    Update sub.c
    normal subtract with positive numbers as answers

--- cfiles/sub.c
+++ cfiles/sub.c
@@ -1,24 +1,26 @@
  // program to subtract two number from user
-#include<stdio.h>
-void main()
-{
-    // variable declaration
-    int a, b, sub=0;       
-    printf(" enter the two digit for sum");
-
-    //input of two numbers from user
-    scanf("%d %d", &a, &b);
-
-    //subtract operation by no. comparision
-    if (a>b)
-    {
-    sub= a-b;
-    }
-   
-    else
-    {
-    sub= b-a;
-    }
+ #include<stdio.h>
+ void main()
+ {
+     // variable declaration
+     int a, b, sub=0;       
+     printf(" enter the two digit for subtract\n");
+ 
+     //input of two numbers from user
+     scanf("%d %d", &a, &b);
+ 
+     //subtract operation by no. comparision
+     if (a>b)
+     {
+     sub= a-b;
+     printf("subtract of numbers: %d", sub);
+     }
     
-
-}
+     else
+     {
+     sub= b-a;
+     printf("subtract of numbers: %d", sub);
+     }
+     
+ 
+ }
