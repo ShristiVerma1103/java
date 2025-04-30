fc2c1c516b646d0308afd4179529b9b20e60f12b
ShristiVerma1103 <shristiverma1103@gmail.com>
2025-04-30 20:22:26 +0530

    Update avg.c
    average of n numbers

--- cfiles/avg.c
+++ cfiles/avg.c
@@ -4,13 +4,13 @@ void main()
     // variable declaration
 
     int n; //  set of no. for average
-    int number, count;
-    float avg, sum=0;
+    int number, count=0,sum=0;
+    float avg ;
 
-    printf(" enter the set of numbers you want average for");
+    printf(" enter the set of numbers you want average for\n");
     scanf("%d", &n);
 
-    printf("enter the numbers");
+    printf("enter the numbers\n");
     while(count < n )
     {
          scanf("%d", &number);
@@ -18,7 +18,7 @@ void main()
          count ++;
 
     }
-   average= sum/n;
+   avg= sum/n;
    printf("average of %d numbers is: %d", n , sum);
-   printf(" therefore the average is: %d", avg);
+   printf(" \ntherefore the average is: %f", avg);
 }
