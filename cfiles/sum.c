24a5559f4ca3758c3799eae25e5397c0923b6283
ShristiVerma1103 <shristiverma1103@gmail.com>
2025-04-30 20:08:48 +0530

    Update sum.c

--- cfiles/sum.c
+++ cfiles/sum.c
@@ -4,12 +4,16 @@ void main()
 {
     // variable declaration
     int a, b, sum=0;       
-    printf(" enter the two digit for sum");
+    printf(" enter the two digit for sum \n ");
 
     //input of two numbers from user
-    scanf("%d %d", &a, &b);
+    scanf(" %d %d", &a, &b);
 
     // sum operation
     sum = a+b;
+
+    //print sum of numbers
+    printf("sum of two numbers :%d",sum);
+
 
 }
