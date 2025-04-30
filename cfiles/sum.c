a796f08ce874b21fa3d919574f519e9ed683290a
ShristiVerma1103 <shristiverma1103@gmail.com>
2025-04-30 19:21:43 +0530

    Update sum.c

---
+++ cfiles/sum.c
@@ -0,0 +1,15 @@
+// program to sum to number from user
+#include<stdio.h>
+void main()
+{
+    // variable declaration
+    int a, b, sum=0;       
+    printf(" enter the two digit for sum");
+
+    //input of two numbers from user
+    scanf("%d %d", &a, &b);
+
+    // sum operation
+    sum = a+b;
+
+}
